e3345030029adcb7f17e66c2cc24aa643d1326fd
rajya_vardhan <rajya_vardhan@epam.com>
2025-05-16 16:26:40 +0530

    uploading folder structre for server

---
+++ server/server.authentication.contracts/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace server.authentication.contracts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ server/server.authentication.data/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace server.authentication.data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
