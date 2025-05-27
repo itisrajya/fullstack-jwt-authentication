e69941e04b1e5a201b0b59d719a0bd6bf7c74667
rajya_vardhan <rajya_vardhan@epam.com>
2025-05-27 23:46:43 +0530

    implementing Repository pattern

---
+++ server/server.authentication.data/IRepository/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using server.authentication.data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.authentication.data.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+        Task AddAsync(User user);
+        Task<bool> IsEmailExistsAsync(string email);
+        Task SaveChangesAsync();
+    }
+}
