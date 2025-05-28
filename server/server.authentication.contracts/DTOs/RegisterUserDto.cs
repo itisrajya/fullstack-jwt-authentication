9a7ebbf32ad86a7c31f6fd2ab30abccaa1ca18f5
rajya_vardhan <rajya_vardhan@epam.com>
2025-05-28 13:26:38 +0530

    adding register user dto

---
+++ server/server.authentication.contracts/DTOs/RegisterUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.authentication.contracts.DTOs
+{
+    public class RegisterUserDto
+    {
+        [Required(ErrorMessage = "The Username field is required.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 20 characters.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        string Email { get; set; }
+        [Required(ErrorMessage = "The Password field is required.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        public string Password { get; set; }
+    }
+}
