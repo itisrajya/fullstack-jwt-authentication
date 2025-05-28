f5a254d60201b199ac3bdcce96c6b0cacd4605b6
rajya_vardhan <rajya_vardhan@epam.com>
2025-05-28 11:27:23 +0530

    adding dns service, adding user check email exsist service, adding valid email service and adding register user service

---
+++ server/server.authentication.application/Service/DnsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.authentication.application.Service
+{
+    public class DnsService
+    {
+        public async Task<IPAddress[]> GetHostEntryAsync(string domain)
+        {
+            var hostEntry = await Dns.GetHostEntryAsync(domain);
+            return hostEntry.AddressList;
+        }
+    }
+}
