638bc65c15bac6776ee84f98e2bee6290bf47b3a
rajya_vardhan <rajya_vardhan@epam.com>
2025-06-16 21:54:42 +0530

    adding JWT token generator service

---
+++ server/server.authentication.application/IService/IJwtTokenGeneratorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.authentication.application.IService
+{
+    public interface IJwtTokenGeneratorService
+    {
+        string GenerateJwtToken(string secretKey, string issuer, string audience, double expirationInMinutes);
+    }
+}
---
+++ server/server.authentication.application/Service/JwtTokenGeneratorService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using server.authentication.application.IService;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.authentication.application.Service
+{
+    public class JwtTokenGeneratorService : IJwtTokenGeneratorService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGeneratorService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateJwtToken(string secretKey, string issuer, string audience, double expirationInMinutes)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, "subject"),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+            };
+            var tokenDescripton = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(expirationInMinutes),
+                signingCredentials: credentials
+            );
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(tokenDescripton);
+        }
+    }
+}
